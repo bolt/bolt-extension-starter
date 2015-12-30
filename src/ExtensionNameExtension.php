5be005012124a10f030b8352cbb63e77e2f29ddd
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-30 12:54:06 +0100

    Update extension class file for Bolt 3

--- src/ExtensionNameExtension.php
+++ src/ExtensionNameExtension.php
@@ -3,18 +3,65 @@
 namespace Bolt\Extension\YourName\ExtensionName;
 
 use Bolt\Application;
-use Bolt\BaseExtension;
+use Bolt\Asset\File\JavaScript;
+use Bolt\Asset\File\Stylesheet;
+use Bolt\Extension\SimpleExtension;
 
-class Extension extends BaseExtension
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class ExtensionNameExtension extends SimpleExtension
 {
-    public function initialize()
-    {
-        $this->addCss('assets/extension.css');
-        $this->addJavascript('assets/start.js', true);
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
-        return "ExtensionName";
+        return 'ExtensionName';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerAssets()
+    {
+        return [
+            new Stylesheet('extension.css'),
+            new JavaScript('extension.js'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigPaths()
+    {
+        return ['templates'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigFunctions()
+    {
+        return [
+            'my_twig_function' => 'myTwigFunction',
+        ];
+    }
+
+    /**
+     * The callback function when {{ my_twig_function() }} is used in a template.
+     *
+     * @return string
+     */
+    public function myTwigFunction()
+    {
+        $context = [
+            'something' => mt_rand(),
+        ];
+
+        return $this->renderTemplate('extension.twig', $context);
     }
 }
